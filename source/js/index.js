ee125610559a3c64b484884d8bdbce80a35a49b7
ap.ilicheva <ap.ilicheva@tensor.ru>
2020-05-26 19:50:55 +0300

    Добавлена инициализация OpenAPI вк + изменено изображение, на более четкое на главной странице

--- source/js/index.js
+++ source/js/index.js
@@ -1,11 +1,31 @@
 import '@/styles/preview-main.scss';
 
+const VK_ACCES = {
+    PHOTOS: 8,
+    GROUP: 262144
+}
+
 window.onload = function () {
+    VK.init({
+        apiId: 7484286
+    });
     const authButton = document.getElementsByClassName('promo-button')[0];
     authButton.onclick = authorization;
 }
 
 function authorization(event) {
-    const window = document.getElementsByClassName('window-auth')[0];
-    window.classList.remove('hidden');
+    VK.Auth.login((response) => {
+            if (response.status === 'connected') { // авторизация прошла успешно
+                const user = {
+                    id: response.session.user.id,
+                    name: response.session.user.first_name,
+                };
+            } else if (response.status === 'not_authorized') { // пользователь авторизован в ВКонтакте, но не разрешил доступ приложению;
+
+            } else if (response.status === 'unknown') { // пользователь не авторизован ВКонтакте.
+
+            }
+
+        }, VK_ACCES.GROUP, VK_ACCES.PHOTOS
+    );
 }
