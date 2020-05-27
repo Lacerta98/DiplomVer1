da19af8758ce10e38129ff6f70fe066dd2f851c9
Настя <ap.ilicheva@tensor.ru>
2020-05-27 03:01:45 +0300

    Полностью сверстанное окно авторизации

--- source/js/index.js
+++ source/js/index.js
@@ -2,10 +2,24 @@ import '@/styles/preview-main.scss';
 
 window.onload = function () {
     const authButton = document.getElementsByClassName('promo-button')[0];
-    authButton.onclick = authorization;
+    const authClose = document.getElementsByClassName('window-auth_form__close')[0];
+    const authForm = document.getElementsByClassName('window-auth_form')[0];
+    authForm.onsubmit = authorization;
+    authButton.onclick = openAuthorization;
+    authClose.onclick = openAuthorization;
 }
 
-function authorization(event) {
+function openAuthorization() {
     const window = document.getElementsByClassName('window-auth')[0];
-    window.classList.remove('hidden');
+    window.classList.toggle('hidden');
 }
+
+function authorization() {
+    const login = document.getElementsByClassName('window-auth_form__login')[0];
+    const password = document.getElementsByClassName('window-auth_form__password')[0];
+    if (login.value && password.value) {
+        alert('Вы отправили форму!');
+    }
+    return false;
+}
+
