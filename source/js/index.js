3a90370c678a4a745fe0ced1f11fa739d73f7166
ap.ilicheva <ap.ilicheva@tensor.ru>
2020-06-04 12:53:30 +0300

    Добавлена инициализация OpenAPI вк + изменено изображение, на более четкое на главной странице

--- source/js/index.js
+++ source/js/index.js
@@ -1,7 +1,5 @@
 import '@/styles/preview-main.scss';
 
-console.log('Все ок');
-
 const VK_ACCESS = {
     PHOTOS: 8,
     GROUP: 262144
@@ -18,16 +16,27 @@ VK.init({
 });
 
 function authorization(event) {
-    setCookie('nast', '123')
-    document.location.href = '/group.html';
+    VK.Auth.login((response) => {
+            if (response.status === 'connected') { // авторизация прошла успешно
+                setCookie('machine_analyst', JSON.stringify(response), {'max-age': 3600});
+                document.location.href = 'http://y906521i.beget.tech/group.html';
+            } else if (response.status === 'not_authorized') { // пользователь авторизован в ВКонтакте, но не разрешил доступ приложению;
+                openWaring();
+            } else if (response.status === 'unknown') { // пользователь не авторизован ВКонтакте.
+                openWaring();
+            }
+
+        }, VK_ACCESS.GROUP
+    );
 }
 
 function openWaring() {
     confirmation.classList.toggle('hidden');
 }
 
-function setCookie(name, value) {
-    let options = {
+function setCookie(name, value, options = {}) {
+
+    options = {
         path: '/',
         // при необходимости добавьте другие значения по умолчанию
         ...options
@@ -48,4 +57,9 @@ function setCookie(name, value) {
     }
 
     document.cookie = updatedCookie;
+    VK.api("groups.get", {user_id: a.session.mid, extended: 1, filter: 'moder', "v":"5.73"}, function (data) {
+        console.log(JSON.stringify(data));
+    });
 }
+
+
